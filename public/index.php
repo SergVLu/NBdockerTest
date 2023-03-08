cfced59203e56d7acb2fad911c43f66fb1a2848c
rumabasy@gmail.com <rumabasy@gmail.com>
2023-03-08 20:49:33 +0300

    add nginx docker

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+  <head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <div>SERG write content</div>
+    <div>This is page index.php</div>
+    <a href="/newhtml.php">SEREGA</a>
+    <?php echo "something by echo" ?>
+  </body>
+</html>
