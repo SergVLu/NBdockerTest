609ac4021f8bd02bbf3780185918aa4390963c1a
SergVLu <rumasyba@gmail.com>
2023-03-11 20:19:01 +0300

    new first commit

--- public/index.php
+++ public/index.php
@@ -1,19 +1,17 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
   <head>
-    <title>TODO supply a title</title>
+    <title>Docker prefille</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
-    <div>SERG write content</div>
+    <div>SERG write content </div>
+    <br>
     <div>This is page index.php</div>
     <a href="/newhtml.php">SEREGA</a>
+    <br>
     <?php echo "something by echo" ?>
   </body>
 </html>
--- public/newhtml.php
+++ public/newhtml.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
   <head>
-    <title>TODO supply a title</title>
+    <title>Support file </title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
-    <div>serega molodets</div>
+    <div>serega molodets</div> 
+    <br>
+
     <a href="/index.php">goto INDEX page</a>
   </body>
 </html>
